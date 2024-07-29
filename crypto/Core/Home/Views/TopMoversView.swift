dd16f6a94e07bec9b308aeaaa7222797ab8df56d
Kushal Patel <patelku@umich.edu>
2024-07-28 21:47:40 -0400

    lazy fetch

--- crypto/Core/Home/Views/TopMoversView.swift
+++ crypto/Core/Home/Views/TopMoversView.swift
@@ -11,15 +11,24 @@ struct TopMoversView: View {
     @StateObject var viewModel: HomeViewModel
 
     var body: some View {
-        VStack (alignment: .leading) {
+        VStack(alignment: .leading) {
             Text("Top Movers")
                 .font(.headline)
-            ScrollView (.horizontal){
-                HStack (spacing: 16) {
-                    ForEach(viewModel.topMovingCoins){ coin in
-                        TopMoversItemView(coin: coin)
+            
+            if (viewModel.topMovingCoins.count != 0) {
+                ScrollView(.horizontal) {
+                    HStack(spacing: 16) {
+                        ForEach(viewModel.topMovingCoins) { coin in
+                            TopMoversItemView(coin: coin)
+                        }
                     }
                 }
+            } else {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                    .scaleEffect(2)
+                    .padding(.vertical, 45)
+                    .padding(.leading, 35)
             }
         }
         .padding()
